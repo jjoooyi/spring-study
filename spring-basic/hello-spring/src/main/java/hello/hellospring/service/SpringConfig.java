88f091609483e66a63c5bd6e24b98f4c3c59db7a
jjoooyi <jjoooyi@gmail.com>
2021-04-25 01:18:48 +0900

    no message

---
+++ spring-basic/hello-spring/src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//직접 Bean 등록하기
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+}
