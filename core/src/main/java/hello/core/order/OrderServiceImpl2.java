5a056002972b818f8737f74b578b43ff589dd068
jjoooyi <jjoooyi@gmail.com>
2021-04-28 22:59:26 +0900

    --210428

--- core/src/main/java/hello/core/order/OrderServiceImpl2.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl2.java
@@ -3,25 +3,34 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
-public class OrderServiceImpl implements OrderService {
+public class OrderServiceImpl2 implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-/* @RequiredArgsConstructor 어노테이션이 만들어주는 생성자!
+    //수정자 주입(setter) 하려면 final 있으면 안됨 수정돼야 돼서
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+
+//    @Autowired(required = false) //필수 등록 안해도 돼! ~선택적~
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl2(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
