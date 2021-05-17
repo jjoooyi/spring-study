c49d623c64a62a3eaf112bb51aef90a21822ae3e
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-05-17 10:47:13 +0900

    spring core project create

---
+++ springcore/src/test/java/me/jooyi/springcore/SpringcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jooyi.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
