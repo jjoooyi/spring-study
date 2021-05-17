6b60cb5d92561c7bf54c9027534f4dd476c2248f
jjoooyi <jjoooyi@gmail.com>
2021-05-17 22:36:42 +0900

    spring-boot-basic study

--- spring-boot/src/main/java/hello/springboot/Application.java
+++ spring-boot/src/main/java/hello/springboot/Application.java
@@ -1,13 +1,35 @@
 package hello.springboot;
 
+import me.jooyi.Holoman;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.File;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Application.class, args);
+
+//		SpringApplication application = new SpringApplication(Application.class);
+//		application.setWebApplicationType(WebApplicationType.NONE);
+//		application.run(args);
+
 	}
+
+//	@Bean
+//	public Holoman holoman() {
+//		Holoman holoman = new Holoman();
+//		holoman.setName("Park");
+//		holoman.setHowLong(45);
+//		return holoman;
+//	}
 
 }
