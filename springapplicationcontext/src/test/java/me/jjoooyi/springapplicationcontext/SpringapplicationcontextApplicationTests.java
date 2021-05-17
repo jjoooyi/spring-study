bd44bd7c01887ceeff903e1c776398a941da6860
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-05-17 10:39:16 +0900

    spring core

---
+++ springapplicationcontext/src/test/java/me/jjoooyi/springapplicationcontext/SpringapplicationcontextApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jjoooyi.springapplicationcontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapplicationcontextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
