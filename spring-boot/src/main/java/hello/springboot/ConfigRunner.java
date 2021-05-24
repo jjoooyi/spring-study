7baa873a37a5f36cc12502a3cc71514c5cd0e6b4
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-05-24 14:22:10 +0900

    Spring boot study --210524

--- spring-boot/src/main/java/hello/springboot/ConfigRunner.java
+++ spring-boot/src/main/java/hello/springboot/ConfigRunner.java
@@ -1,19 +1,20 @@
 package hello.springboot;
 
-//import me.jooyi.Holoman;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HolomanRunner implements ApplicationRunner {
+public class ConfigRunner implements ApplicationRunner {
 
-//    @Autowired
-//    Holoman holoman;
+    @Autowired
+    private String hello;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-//        System.out.println(holoman);
+        System.out.println("==========================");
+        System.out.println(hello);
+        System.out.println("==========================");
     }
 }
