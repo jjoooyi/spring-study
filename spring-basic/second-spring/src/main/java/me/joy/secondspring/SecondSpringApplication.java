6e0acd5e59d4aaf54a0b4628ba38f87af766bd05
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-06-08 12:57:29 +0900

    study  basic 2nd-time

---
+++ spring-basic/second-spring/src/main/java/me/joy/secondspring/SecondSpringApplication.java
@@ -0,0 +1,13 @@
+package me.joy.secondspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSpringApplication.class, args);
+	}
+
+}
