01d6b272523bec8804779187cf3af19163cdd572
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-06-08 11:06:56 +0900

    stream grouping

--- java-study/src/ex_stream/Ex11Grouping.java
+++ java-study/src/ex_stream/Ex11Grouping.java
@@ -1,11 +1,9 @@
 package ex_stream;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Stream;
 
+import static java.util.Comparator.comparingInt;
 import static java.util.stream.Collectors.*;
 
 class Student3 {
@@ -55,26 +53,117 @@ class Student3 {
 
 public class Ex11Grouping {
     public static void main(String[] args) {
-        Student2[] stuArr = {
-                new Student2("A", true, 1, 1, 300),
-                new Student2("B", false, 1, 1, 250),
-                new Student2("C", true, 1, 1, 200),
-                new Student2("D", false, 1, 2, 150),
-                new Student2("E", true, 1, 2, 100),
-                new Student2("F", false, 1, 2, 50),
-                new Student2("G", true, 1, 3, 100),
-                new Student2("H", false, 1, 3, 150),
-                new Student2("I", true, 1, 3, 200),
-                new Student2("J", false, 2, 1, 350),
-                new Student2("K", true, 2, 1, 250),
-                new Student2("L", false, 2, 1, 200),
-                new Student2("M", true, 2, 2, 150),
-                new Student2("N", false, 2, 2, 100),
-                new Student2("O", true, 2, 2, 50),
-                new Student2("P", false, 2, 3, 100),
-                new Student2("Q", true, 2, 3, 150),
-                new Student2("R", false, 2, 3, 200)
+        Student3[] stuArr = {
+                new Student3("A", true, 1, 1, 300),
+                new Student3("B", false, 1, 1, 250),
+                new Student3("C", true, 1, 1, 200),
+                new Student3("D", false, 1, 2, 150),
+                new Student3("E", true, 1, 2, 100),
+                new Student3("F", false, 1, 2, 50),
+                new Student3("G", true, 1, 3, 100),
+                new Student3("H", false, 1, 3, 150),
+                new Student3("I", true, 1, 3, 200),
+                new Student3("J", false, 2, 1, 350),
+                new Student3("K", true, 2, 1, 250),
+                new Student3("L", false, 2, 1, 200),
+                new Student3("M", true, 2, 2, 150),
+                new Student3("N", false, 2, 2, 100),
+                new Student3("O", true, 2, 2, 50),
+                new Student3("P", false, 2, 3, 100),
+                new Student3("Q", true, 2, 3, 150),
+                new Student3("R", false, 2, 3, 200)
         };
+
+        System.out.println("1. 단순 그룹화(반별로 그룹화");
+        Map<Integer, List<Student3>> stuByBan = Stream.of(stuArr)
+                .collect(groupingBy(Student3::getBan));
+
+        for (List<Student3> ban : stuByBan.values()) {
+            for (Student3 s : ban) {
+                System.out.println(s);
+            }
+        }
+
+        //===================================================
+        System.out.println("2. 단순 그룹화(성적별로 그룹화)");
+        Map<Student3.Level, List<Student3>> stuByLevel = Stream.of(stuArr)
+                .collect(groupingBy(s -> {
+                    if(s.getScore() >= 200) return Student3.Level.HIGH;
+                    else if(s.getScore()>=100) return Student3.Level.MID;
+                    else return Student3.Level.LOW;
+                }));
+
+        TreeSet<Student3.Level> keySet = new TreeSet<>(stuByLevel.keySet());
+
+        for (Student3.Level key : keySet) {
+            System.out.println("[" + key + "]");
+
+            for (Student3 s : stuByLevel.get(key)) {
+                System.out.println(s);
+            }
+            System.out.println();
+        }
+
+        //===================================================
+        System.out.println("3. 단순그룹화 + 통계(성적별 학생수)");
+        Map<Student3.Level, Long> stuCntByLevel = Stream.of(stuArr)
+                .collect(groupingBy(s -> {
+                    if(s.getScore() >= 200) return Student3.Level.HIGH;
+                    else if(s.getScore()>=100) return Student3.Level.MID;
+                    else return Student3.Level.LOW;
+                }, counting()));
+
+        for (Student3.Level key : stuCntByLevel.keySet()) {
+            System.out.printf("[%s] - %d명, ", key, stuCntByLevel.get(key));
+        }
+        System.out.println();
+
+        //===================================================
+        System.out.println("4. 다중그룹화(학년별, 반별)");
+        Map<Integer, Map<Integer, List<Student3>>> stuByHBakAndBan = Stream.of(stuArr)
+                .collect(groupingBy(Student3::getHak,
+                        groupingBy(Student3::getBan)));
+
+        for (Map<Integer, List<Student3>> hak : stuByHBakAndBan.values()) {
+            for (List<Student3> ban : hak.values()) {
+                System.out.println();
+                for (Student3 s : ban) {
+                    System.out.println(s);
+                }
+            }
+        }
+
+        //===================================================
+        System.out.println("5. 다중그룹화 + 통계(학년별, 반별 1등)");
+        Map<Integer, Map<Integer, Student3>> topStuByHakAndBan = Stream.of(stuArr)
+                .collect(groupingBy(Student3::getHak,
+                        groupingBy(Student3::getBan,
+                                collectingAndThen(
+                                        maxBy(comparingInt(Student3::getScore))
+                                        , Optional::get
+                                ))));
+
+        for (Map<Integer, Student3> ban : topStuByHakAndBan.values()) {
+            for (Student3 s : ban.values()) {
+                System.out.println(s);
+            }
+        }
+
+        //===================================================
+        System.out.println("6. 다중그룹화 + 통계(학년별, 반별 성적그룹)");
+        Map<String,Set<Student3.Level>> stuByScoreGroup = Stream.of(stuArr)
+                .collect(groupingBy(s -> s.getHak() + "-" + s.getBan(), //문자열로 분류하여 그루핑
+                        mapping(s -> {
+                            if(s.getScore() >= 200) return Student3.Level.HIGH;
+                            else if(s.getScore()>=100) return Student3.Level.MID;
+                            else return Student3.Level.LOW;
+                        }, toSet())));
+
+        Set<String> keySet2 = stuByScoreGroup.keySet();
+
+        for (String key : keySet2) {
+            System.out.println("["+key+"]"+stuByScoreGroup.get(key));
+        }
 
 
     }
