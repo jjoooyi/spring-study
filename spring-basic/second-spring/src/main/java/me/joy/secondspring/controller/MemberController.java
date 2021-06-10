594f93d104e6e960d26b01be0b70090011516219
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-06-10 10:55:20 +0900

    no message

--- spring-basic/second-spring/src/main/java/me/joy/secondspring/controller/MemberController.java
+++ spring-basic/second-spring/src/main/java/me/joy/secondspring/controller/MemberController.java
@@ -1,10 +1,16 @@
 package me.joy.secondspring.controller;
 
+import me.joy.secondspring.domain.Member;
 import me.joy.secondspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
@@ -12,5 +18,24 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("members", memberService.findMembers());
+        return "members/memberList";
     }
 }
