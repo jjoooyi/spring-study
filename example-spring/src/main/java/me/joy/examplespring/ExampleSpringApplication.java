88b90fc773575206c4e9ef5f2b8d935e24c11b72
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-06-10 11:24:49 +0900

    no message

---
+++ example-spring/src/main/java/me/joy/examplespring/ExampleSpringApplication.java
@@ -0,0 +1,13 @@
+package me.joy.examplespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringApplication.class, args);
+	}
+
+}
---
+++ example-spring/src/test/java/me/joy/examplespring/ExampleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joy.examplespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
