ddd36a1928c577202e08df26563704571b43df95
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-06-18 11:04:20 +0900

    core study 2nd time

---
+++ core2nd/src/main/java/me/joy/core2nd/Core2ndApplication.java
@@ -0,0 +1,13 @@
+package me.joy.core2nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Core2ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Core2ndApplication.class, args);
+	}
+
+}
---
+++ core2nd/src/test/java/me/joy/core2nd/Core2ndApplicationTests.java
@@ -0,0 +1,13 @@
+package me.joy.core2nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Core2ndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
