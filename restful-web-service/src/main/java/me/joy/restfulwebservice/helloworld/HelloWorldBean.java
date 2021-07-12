d79e00b963706e0f15bb587440264e4f60cd6f3e
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-07-12 17:51:53 +0900

    restful web service

---
+++ restful-web-service/src/main/java/me/joy/restfulwebservice/helloworld/HelloWorldBean.java
@@ -0,0 +1,12 @@
+package me.joy.restfulwebservice.helloworld;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+}
---
+++ restful-web-service/src/main/java/me/joy/restfulwebservice/user/UserController.java
@@ -0,0 +1,8 @@
+package me.joy.restfulwebservice.user;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private UserDaoService service;
+}
