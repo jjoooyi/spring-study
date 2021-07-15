9f76f3ca0cc1aaefd2fa1e507a9c423534b9c7d9
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-07-15 17:48:07 +0900

    restful-web-service

--- restful-web-service/src/main/java/me/joy/restfulwebservice/user/UserController.java
+++ restful-web-service/src/main/java/me/joy/restfulwebservice/user/UserController.java
@@ -1,8 +1,71 @@
 package me.joy.restfulwebservice.user;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
-    private UserDaoService service;
+
+    private final UserDaoService service;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    // GET /users/1 or /users/10
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@Valid  @RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+    }
+
+    @PutMapping("/users/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+
+        User deletedUser = service.deleteById(id);
+        if (deletedUser == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        User updatedUser = service.save(user);
+        if (updatedUser == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
