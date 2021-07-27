2607f08efd08c0391be22bbda4f074d80aac75aa
LAPTOP-LHPVDHS1\TA9 <jjoooyi@gmail.com>
2021-07-27 17:57:18 +0900

    blog project -- join / use ajax, js.. api...

--- blog/src/main/java/me/jooy/blog/controller/UserController.java
+++ blog/src/main/java/me/jooy/blog/controller/UserController.java
@@ -1,4 +1,21 @@
 package me.jooy.blog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/join")
+    public String joinForm() {
+        return "users/joinForm";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "users/loginForm";
+    }
+
 }
---
+++ blog/src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<br />
+<div class="jumbotron text-center" style="margin-bottom:0">
+    <p>Created by Joy🥨</p>
+    <p>👓jjoooyi@gmail.com</p>
+    <p>New York✨</p>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
